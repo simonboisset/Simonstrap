46f5dc3874f2652dd64f440611a45eb9d384607b
Simon Boisset <simon.boisset@gmail.com>
2019-04-16 01:17:41 +0200

    Update Dialog.js

--- src/imports/Dialog.js
+++ src/imports/Dialog.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import {basic} from "../themes/basic";
 class Dialog extends React.Component {
   render() {
     let visibility,opacity,top;
@@ -34,7 +35,7 @@ class Dialog extends React.Component {
         left :"0",
         right :"0",
         bottom :"0",
-        backgroundColor :"rgba(0,0,0,0.4)",
+        backgroundColor :"rgba(0,0,0,0.1)",
         zIndex :"998",
         opacity : opacity,
         transition: `opacity ${this.props.delay} linear`,
@@ -42,11 +43,13 @@ class Dialog extends React.Component {
       box:{
         position : "fixed",
         top:top,
-        width :"100px",
-        height :"100px",
+        width :this.props.width,
+        height :this.props.height,
         backgroundColor :"white",
         zIndex :"999",
         transition : `top ${this.props.delay} linear`,
+        borderRadius: this.props.theme.borderRadius.default,
+        boxShadow:this.props.theme.elevation[this.props.elevation]
       }
     }
     return(
@@ -58,12 +61,20 @@ class Dialog extends React.Component {
   }
 }
 Dialog.defaultProps = {
-  delay: "300ms"
+  width:"350px",
+  height:"200px",
+  delay: "300ms",
+  theme:basic,
+  elevation:2
 };
 Dialog.propTypes = {
+  width:PropTypes.string,
+  height:PropTypes.string,
   delay: PropTypes.string,
   active: PropTypes.bool,
   onClose: PropTypes.func,
+  theme:PropTypes.object,
+  elevation:PropTypes.number
 };
 
 export default Dialog;
