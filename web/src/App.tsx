9949128182b9d654189eeab4cfa99b35b6ed2bd7
Simon Boisset <simon.boisset@gmail.com>
2020-11-01 00:57:59 +0100

    Dev (#26)
    * delete exemple
    
    * rigth icon
    
    * Update App.tsx
    
    * v0
    
    * Update ci.yml

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -5,9 +5,9 @@ function App() {
   return (
     <div className="App">
       <AppBar
-        leftElements={<IconButton>home</IconButton>}
+        leftElements={<IconButton>menu</IconButton>}
         rigthElements={<IconButton>home</IconButton>}
-        title="Simon UI"
+        title="Simon UI v0"
       />
     </div>
   );
